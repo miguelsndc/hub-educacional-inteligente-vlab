3cffb303ea3711f216092027a5a20c45f2fbcf8b
miguelsndc <mglsndc@gmail.com>
2026-03-02 14:26:09 -0300

    backend: consertar protocols.py flake8

--- backend/app/providers/protocols.py
+++ backend/app/providers/protocols.py
@@ -2,8 +2,7 @@ from typing import Protocol
 from app.schemas.ai import AIResponse
 
 
-"""
-Define a interface do provider de IA usando Protocols para garantir 
+"""Define a interface do provider de IA usando Protocols para garantir
 que qualquer implementação tenha o método suggest com a assinatura correta.
 Isso permite que o sistema seja flexível e possa facilmente integrar diferentes
 provedores de IA no futuro, desde que eles implementem essa interface, também
@@ -11,8 +10,7 @@ permitindo facilmente mockar o provider para testes
 
 A acoplação entre o schema e o provider é feita através do tipo de retorno do suggest(...), que é AIResponse.
 Isso garante que qualquer mudança no schema tenha que ser refletida na implementação do provider,
-mantendo a consistência entre cada parte do sistema
-"""
+mantendo a consistência entre cada parte do sistema"""
 
 
 class AIProvider(Protocol):
