3603b0051871aa5a6eca64cbcd960c15c95a8322
miguelsndc <mglsndc@gmail.com>
2026-03-03 19:40:11 -0300

    frontend: setar o arquivo de entrada

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,20 @@
-function App() {
-  return <div>ok</div>
-}
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { FormPage } from "./pages/Form";
+import { ListPage } from "./pages/List";
 
-export default App
+const queryClient = new QueryClient();
+
+export default function App() {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<ListPage />} />
+                    <Route path="/new" element={<FormPage />} />
+                    <Route path="/edit/:id" element={<FormPage />} />
+                </Routes>
+            </BrowserRouter>
+        </QueryClientProvider>
+    );
+}
